b0daa9d4f28164c4dbce68410639e45d04aa1475
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-11-16 17:43:19 +0100

    updated new changes to file

--- Terraform/terrapro2/vars.tf
+++ Terraform/terrapro2/vars.tf
@@ -1,15 +1,15 @@
-variable REGION {
-    default = "us-east-1"
+variable "REGION" {
+  default = "us-east-1"
 }
 
-variable ZONE1 {
-    default = "us-east-1b"
+variable "ZONE1" {
+  default = "us-east-1b"
 }
 
-variable AMIS {
-    type = map
-    default = {
-        us-east-1 = "ami-0fc5d935ebf8bc3bc"
-        us-west-1 = "ami-06e4ca05d431835e9"
-    }
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0fc5d935ebf8bc3bc"
+    us-west-1 = "ami-06e4ca05d431835e9"
+  }
 }
