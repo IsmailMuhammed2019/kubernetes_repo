380fe592aee45832b3ace59401132ed174c533d0
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-11-17 08:43:11 +0100

    update key name with variable key

--- Terraform/terrapro2/instance.tf
+++ Terraform/terrapro2/instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "proterratest" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
-  key_name               = "kops-key"
+  key_name               = var.KEYS
   vpc_security_group_ids = ["sg-0268673e6e1ed12ad"]
   tags = {
     Name = "project1"
