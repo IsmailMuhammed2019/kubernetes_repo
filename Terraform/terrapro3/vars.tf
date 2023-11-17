db93520c6df428a005abc44c299761faff3dc228
ismailmuhammed <ismailmuhammed2019@gmail.com>
2023-11-17 10:37:11 +0100

    update AmI id to amazon ami id

--- Terraform/terrapro3/vars.tf
+++ Terraform/terrapro3/vars.tf
@@ -9,7 +9,7 @@ variable "ZONE1" {
 variable "AMIS" {
   type = map(any)
   default = {
-    us-east-1 = "ami-0fc5d935ebf8bc3bc"
+    us-east-1 = "ami-0230bd60aa48260c6"
     us-west-1 = "ami-06e4ca05d431835e9"
   }
 }
