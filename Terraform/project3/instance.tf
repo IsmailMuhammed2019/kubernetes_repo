29f039428bdf3ab7b9860f06ff2f70fa47090017
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-11-22 17:21:15 +0100

    create provider file for region

---
+++ Terraform/project3/instance.tf
@@ -0,0 +1,33 @@
+resource "aws_key_pair" "ubuntu-key" {
+  key_name   = "ubuntu-key"
+  public_key = file("ubuntukey.pub")
+}
+
+resource "aws_instance" "webtemplate" {
+  ami                    = var.AMIS[var.REGION]
+  instance_type          = "t2.micro"
+  availability_zone      = var.ZONE1
+  key_name               = aws_key_pair.ubuntu-key.key_name
+  vpc_security_group_ids = ["sg-0268673e6e1ed12ad"]
+  tags = {
+    Name = "webtemplate"
+  }
+
+  provisioner "file" {
+    source      = "app.sh"
+    destination = "/tmp/app.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod u+x /tmp/app.sh",
+      "sudo /tmp/app.sh",
+    ]
+  }
+
+  connection {
+    user        = var.USER
+    private_key = file("ubuntukey")
+    host        = self.public_ip
+  }
+}
